207dd834a86e4723bb9e5e32101deb327b1f5d14
HasinduLanka <hlrathnayaka@gmail.com>
2021-12-13 00:53:34 +0530

    Next.js

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main>
+        <img src="/images/bitz.png" height="100rem" width="100rem" alt="Bitzqaud" />
+
+        <h1 className="text-blue-500">
+          Sample{' '}
+          <Link href="/posts/first-post">
+            <a>Link to a page</a>
+          </Link>
+        </h1>
+
+        <p className="text-sky-600 text-xl ">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+        <br />
+        <p>
+          Add new pages to <font className="code">/pages</font> as <font className="code">.js</font> files and they'll automatically appear in our website.
+          <br />
+          For example, you can create a <font className="code" >pages/post/bitz.js</font> and it will generate <font className="code" >bitzquad.com/post/bitz.html</font>
+        </p>
+
+
+      </main>
+
+      <style jsx>{`
+      .code {
+        font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+        color: #444;
+        background-color: #f6f8fa;
+        padding: 0.2em 0.4em;
+        border-radius: 3px;
+      }
+    `}</style>
+
+    </div>
+
+
+
+
+  )
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
   ]
-};
+}
