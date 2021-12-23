df61b3d88d8b30432479774dc16f323f560e004d
hariwij <hariwijesinghe234@gmail.com>
2021-12-23 14:49:14 +0530

    Add Sections as Compnents, Add Pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { GetLanguage } from "../siteUtils";
+  
+    let lang = GetLanguage($page.params.pages);
+  
+    let Txt = {
+      About: {
+        en: "This is the minimal sample page",
+        fr: "Ceci est la page d'exemple minimale",
+        si: "මෙය පළමු උපදාරයේ පිටුවක් පෙන්වා ඇත",
+      },
+    };
+  </script>
+  
+  <svelte:head>
+  <title>About Us - Bitzquad | Software and IT Solutions</title>
+</svelte:head>
+
+  <h1>About Us</h1>
+  <p>This text will be same on all languages</p>
+  
+  <p>{Txt.About[lang]}</p>
+  
