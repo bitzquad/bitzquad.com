af4fead9447e95438e9383fe2165e1d6de8e47ff
hariwij <hariwijesinghe234@gmail.com>
2021-12-28 08:34:08 +0000

    Design Techstack section

---
+++ src/lib/Techstack.svelte
@@ -0,0 +1,24 @@
+<div
+    class="p-24 flex justify-center align-center"
+    style="width: 100vw; height:100vh;  "
+>
+    <div
+        class="bg-white shadow shadow-slate-400 absolute"
+        style="border-radius: 7vw; width:7vw; height:7vw;left:30%; transform:translate(0% , -30%); background-image: url('\react-icon.svg'); background-repeat: no-repeat; background-position: center; background-size: contain;"
+    />
+    <div class="relative" style="width: 100%; height: 100%; background-image: url('\techstack.svg'); background-repeat: no-repeat; background-position: center;">
+        <h1 class="text-center text-gray-500 font-semibold" style="font-size: 3vw;">
+            Our Techstack
+        </h1>
+        <div class="bottom-0 absolute" style="width: 100%;">
+            <h6 class="text-center text-gray-500 text-xl">
+                We evolve with the world to acquire cutting edge technology for
+                you to stay ahead of others stuck on decades old software.
+            </h6>
+        </div>
+    </div>
+</div>
+
+<style>
+    
+</style>
--- src/routes/[pages]/SamplePage.svelte
+++ src/routes/[pages]/SamplePage.svelte
@@ -1,27 +1,8 @@
 <script lang="ts" context="module">
+    import Techstack from "$lib/Techstack.svelte";
   import { page } from "$app/stores";
   import { lang, Path, PathForLanguage } from "$lib/siteUtils";
   import Txt from "./SamplePage-Text";
 </script>
 
-<div>
-  <h1>{Txt.Welcome[lang]}</h1>
-
-  <br />
-
-  <p>This text will be same on all languages</p>
-
-  <p>{Txt.About[lang]}</p>
-</div>
-
-<div>
-  Change language :
-
-  <a href={PathForLanguage($page.path, "en")}>English</a>
-  <a href={PathForLanguage($page.path, "si")}>Sinhala</a>
-  <a href={PathForLanguage($page.path, "fr")}>French</a>
-</div>
-
-<div>
-  <a href={Path("/SimplePage")}>Go to SimplePage</a>
-</div>
+<Techstack></Techstack>
