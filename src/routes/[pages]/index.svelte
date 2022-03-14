a4f3db99707d2448194714997a6cbce057b02c95
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-03-14 15:17:41 +0530

    :art: fix: improve layout structure

--- src/routes/[pages]/index.svelte
+++ src/routes/[pages]/index.svelte
@@ -1,76 +1,77 @@
 <script>
-  import Footer from "$lib/Footer.svelte";
-  import HeroSection from "$lib/Home/HeroSection.svelte";
-  import Navbar from "$lib/Navbar.svelte";
-  import { lang, Path } from "$lib/siteUtils";
+	import Footer from '$lib/Footer.svelte';
+	import HeroSection from '$lib/Home/HeroSection.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import { lang, Path } from '$lib/siteUtils';
 
-  let Txt = {
-    Slogun: {
-      en: "Solutions beyond Software Engineering",
-      fr: "Nous sommes une équipe de passionnés qui adore créer.",
-      si: "Api thamai hodatama karanne.",
-    },
-    GearingUp: {
-      en: "We are currently building infrastructure, preparing dev tools, and winding up servers for the next technological advancement of your business",
-      fr: "Nous sommes en train de construire l'infrastructure, préparer les outils de développement, et terminer les serveurs pour la prochaine technologie de votre entreprise",
-      si: "Thaama hadana gaman",
-    },
-    LaunchingSoon: {
-      en: "The date, we are launching our services for you",
-      fr: "La date de lancement de nos services pour vous",
-      si: "Me davase aewith balanna",
-    },
-  };
+	let Txt = {
+		Slogun: {
+			en: 'Solutions beyond Software Engineering',
+			fr: 'Nous sommes une équipe de passionnés qui adore créer.',
+			si: 'Api thamai hodatama karanne.',
+		},
+		GearingUp: {
+			en: 'We are currently building infrastructure, preparing dev tools, and winding up servers for the next technological advancement of your business',
+			fr: "Nous sommes en train de construire l'infrastructure, préparer les outils de développement, et terminer les serveurs pour la prochaine technologie de votre entreprise",
+			si: 'Thaama hadana gaman',
+		},
+		LaunchingSoon: {
+			en: 'The date, we are launching our services for you',
+			fr: 'La date de lancement de nos services pour vous',
+			si: 'Me davase aewith balanna',
+		},
+	};
 </script>
 
 <svelte:head>
-  <title>Bitzquad | Software and IT Solutions</title>
+	<title>Bitzquad | Software and IT Solutions</title>
 
-  <meta name="title" content="Bitzquad | Software and IT Solutions" />
-  <meta
-    name="description"
-    content="Bitzquad creates next generation future-proof digital solutions for you."
-  />
-  <meta
-    name="keywords"
-    content="bitzquad, bitsquad, software, solutions, website"
-  />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta name="language" content="English" />
-  <meta name="author" content="Bitzquad" />
+	<meta name="title" content="Bitzquad | Software and IT Solutions" />
+	<meta
+		name="description"
+		content="Bitzquad creates next generation future-proof digital solutions for you."
+	/>
+	<meta
+		name="keywords"
+		content="bitzquad, bitsquad, software, solutions, website"
+	/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="English" />
+	<meta name="author" content="Bitzquad" />
 
-  <!-- Open Graph / Facebook Meta   -->
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://bitzquad.com/" />
-  <meta property="og:title" content="Bitzquad | Software and IT Solutions" />
-  <meta
-    property="og:description"
-    content="Bitzquad creates next generation future-proof digital solutions for you."
-  />
-  <meta
-    property="og:image"
-    content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
-  />
+	<!-- Open Graph / Facebook Meta   -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://bitzquad.com/" />
+	<meta property="og:title" content="Bitzquad | Software and IT Solutions" />
+	<meta
+		property="og:description"
+		content="Bitzquad creates next generation future-proof digital solutions for you."
+	/>
+	<meta
+		property="og:image"
+		content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
+	/>
 
-  <!-- Twitter Meta tags   -->
-  <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://bitzquad.com/" />
-  <meta
-    property="twitter:title"
-    content="Bitzquad | Software and IT Solutions"
-  />
-  <meta
-    property="twitter:description"
-    content="Bitzquad creates next generation future-proof digital solutions for you."
-  />
-  <meta
-    property="twitter:image"
-    content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
-  />
+	<!-- Twitter Meta tags   -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://bitzquad.com/" />
+	<meta
+		property="twitter:title"
+		content="Bitzquad | Software and IT Solutions"
+	/>
+	<meta
+		property="twitter:description"
+		content="Bitzquad creates next generation future-proof digital solutions for you."
+	/>
+	<meta
+		property="twitter:image"
+		content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
+	/>
 </svelte:head>
-
+<header>
+	<Navbar />
+</header>
 <main class="main">
-  <Navbar />
-  <HeroSection />
-  <Footer />
+	<HeroSection />
 </main>
+<Footer />
--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,52 +1,52 @@
 <script context="module">
-  import Footer from '$lib/Footer.svelte';
-  import Navbar from '$lib/Navbar.svelte';
-  import '../app.scss';
-  /** @type {import('@sveltejs/kit').ErrorLoad} */
-  export function load({ error, status }) {
-    return {
-      props: {
-        error,
-        status,
-      },
-    };
-  }
+	import Footer from '$lib/Footer.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import '../app.scss';
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				error,
+				status,
+			},
+		};
+	}
 </script>
 
 <script>
-  export let error;
-  export let status;
+	export let error;
+	export let status;
 </script>
 
 <div class="w-screen">
-  <Navbar />
-  <div
-    class="bz-container py-4 px-5 md:px-10 lg:px-20 bg-gray-100 text-gray-500 "
-  >
-    <div
-      class="container max-w-7xl flex items-center mt-12 justify-center flex-col"
-    >
-      <h1 class="text-4xl font-bold">
-        {status === 404 ? 'Page Not Found! ' : `Server Error ${status}`}
-      </h1>
-      <p class="md:w-1/2 text-center mt-5">
-        {status === 404
-          ? "Page you're trying reach dose not exsits. Or content may be moved to another page, or you don't have access to view requested content."
-          : 'an error occured on our side. Our Engineers are working on it!'}
-      </p>
-      <div class="md:relative w-full md:w-1/2 mt-10 mb-5 flex flex-col-reverse">
-        <a
-          href="/"
-          class="md:absolute my-10 md:mt-0 w-full md:w-auto md:top-0 md:left-1/2 flex justify-center  md:-translate-x-1/2 bg-yellow-400 text-yellow-700 p-3 px-4 rounded hover:text-gray-600 transition-all duration-300 shadow-lg hover:bg-yellow-300 hover:shadow-xl"
-          >Go To Home</a
-        >
-        <img
-          src={status === 404 ? '/404-error.svg' : '/bug-fix.svg'}
-          alt="404-error"
-          class="w-full"
-        />
-      </div>
-    </div>
-  </div>
-  <Footer />
+	<Navbar />
+	<div
+		class="bz-container py-4 px-5 md:px-10 lg:px-20 bg-gray-100 text-gray-500 "
+	>
+		<div
+			class="container max-w-7xl flex items-center mt-12 justify-center flex-col"
+		>
+			<h1 class="text-4xl font-bold">
+				{status === 404 ? 'Page Not Found! ' : `Server Error ${status}`}
+			</h1>
+			<p class="md:w-1/2 text-center mt-5">
+				{status === 404
+					? "Page you're trying reach dose not exsits. Or content may be moved to another page, or you don't have access to view requested content."
+					: 'an error occured on our side. Our Engineers are working on it!'}
+			</p>
+			<div class="md:relative w-full md:w-1/2 mt-10 mb-5 flex flex-col-reverse">
+				<a
+					href="/"
+					class="md:absolute my-10 md:mt-0 w-full md:w-auto md:top-0 md:left-1/2 flex justify-center  md:-translate-x-1/2 bg-yellow-400 text-yellow-700 p-3 px-4 rounded hover:text-gray-600 transition-all duration-300 shadow-lg hover:bg-yellow-300 hover:shadow-xl"
+					>Go To Home</a
+				>
+				<img
+					src={status === 404 ? '/404-error.svg' : '/bug-fix.svg'}
+					alt="404-error"
+					class="w-full"
+				/>
+			</div>
+		</div>
+	</div>
+	<Footer />
 </div>
