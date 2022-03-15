ded00592a6c4b430d840e4a562c59d1b6491ff9c
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-03-15 11:28:29 +0530

    :bug: build: fix all build errors

--- src/routes/[pages]/social/[index].svelte
+++ src/routes/[pages]/social/[index].svelte
@@ -1,25 +1,24 @@
 <script context="module">
-    
-  const social = {
-    facebook: 'https://www.facebook.com/bitzquad/',
-    linkedin: 'https://www.linkedin.com/company/bitzquad/',
-    instagram: '',
-    twitter: '',
-    youtube: '',
-    github: 'https://github.com/bitzquad/',
-    behance: '',
-  };
+	const social = {
+		facebook: 'https://www.facebook.com/bitzquad/',
+		linkedin: 'https://www.linkedin.com/company/bitzquad/',
+		instagram: 'https://www.instagram.com/',
+		twitter: 'https://twitter.com/',
+		youtube: 'https://www.youtube.com/',
+		github: 'https://github.com/bitzquad/',
+		behance: 'https://www.behance.net/Bitzquad',
+	};
 
-  export async function load({ params }) {
-    if (social[params.index]) {
-      return {
-        status: 302,
-        redirect: social[params.index],
-      };
-    } else {
-      return {
-        status: 404,
-      };
-    }
-  }
+	export async function load({ params }) {
+		if (social[params.index]) {
+			return {
+				status: 302,
+				redirect: social[params.index],
+			};
+		} else {
+			return {
+				notFound: true,
+			};
+		}
+	}
 </script>
