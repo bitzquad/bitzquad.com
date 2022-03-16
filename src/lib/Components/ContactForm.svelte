ef45d798303f6cb7a02853d0dff45f6a5f35b5e1
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-03-16 16:49:16 +0530

    🐛:fix: Rename Message section and Improve styles

---
+++ src/lib/Components/ContactForm.svelte
@@ -0,0 +1,78 @@
+<div class="bg-slate-100 w-full py-10 lg:py-20 bz-container">
+    <div class="bg-slate-100 w-full container max-w-7xl">
+      <div>
+        <h3 class="text-3xl lg:text-4xl   text-center">Get in Touch</h3>
+        <p class="py-2 text-center">Amet minim mollit non deserunt</p>
+      </div>
+      <div class="px-5 w-full flex lg:flex-row">
+        <img
+          src="/contact-form.svg"
+          alt="contact-form"
+          class="hidden lg:block lg:w-1/2 lg:h-auto px-20"
+        />
+        <form class="contact-form">
+          <div class="group relative  z-0 mb-6 w-full">
+            <input
+              type="text"
+              name="floating_name"
+              class="peer contact-form-input "
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_name"
+              class="contact-form-input-label"
+            >
+              Name
+            </label>
+          </div>
+          <div class="group relative  z-0 mb-6 w-full">
+            <input
+              type="text"
+              name="floating_email"
+              class="peer contact-form-input"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_email"
+              class="contact-form-input-label"
+            >
+              Email
+            </label>
+          </div>
+          <div class="group relative  z-0 mb-6 w-full">
+            <input
+              type="text"
+              name="floating_subject"
+              class="peer contact-form-input"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_subject"
+              class="contact-form-input-label"           
+               >
+              Subject
+            </label>
+          </div>
+          <div class="group relative  z-0 mb-6 w-full">
+            <textarea
+              name="floating_message"
+              class="peer contact-form-input"              
+              placeholder=" "
+              rows="4"
+              required
+            />
+            <label
+              for="floating_message"
+              class="contact-form-input-label"           
+               >
+              Your Message
+            </label>
+          </div>
+          <button class="bz-button w-full rounded  py-2 text-center">Send Message</button>
+        </form>
+      </div>
+    </div>
+  </div>
