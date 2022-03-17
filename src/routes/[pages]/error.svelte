af901a5bd0d37ea9f1d71e691d8cc44f81eeda2e
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-03-17 23:12:17 +0530

    :bug: error.svelte

--- src/routes/[pages]/error.svelte
+++ src/routes/[pages]/error.svelte
@@ -2,12 +2,15 @@
 	import {
 		Path,
 		GetLanguageFromPath,
-		lang as langOfPath,
+		lang,
+		PathForLanguage,
 	} from "$lib/siteUtils";
 
 	import { browser } from "$app/env";
+	import Footer from "$lib/Footer.svelte";
+	import Navbar from "$lib/Navbar.svelte";
 
-	let lang = langOfPath;
+	import Txt from "./ErrorText";
 
 	export async function load({ fetch }) {
 		if (browser) {
@@ -15,69 +18,63 @@
 				window.location.search &&
 				window.location.search.startsWith("?")
 			) {
-				lang = GetLanguageFromPath(window.location.search.slice(1));
+				let origlang = GetLanguageFromPath(window.location.pathname);
+				let queryLang = GetLanguageFromPath(
+					window.location.search.slice(1)
+				);
+
+				if (queryLang && queryLang !== origlang) {
+					console.log("redirecting");
+					window.location.replace(
+						PathForLanguage(
+							"/Error" + window.location.search,
+							queryLang
+						)
+					);
+				}
 			}
 		} else {
 			// Runs on server
 		}
 		return {};
 	}
-
-	let Txt = {
-		Msg: {
-			en: "404 not found",
-			fr: "404 not found in French",
-			si: "404 not found in Sinhala",
-		},
-	};
 </script>
 
-<svelte:head>
-	<title>{Txt.Msg[lang]}</title>
+<script>
+	export let statusCode = 404;
+</script>
 
-	<meta name="title" content="Bitzquad | Software and IT Solutions" />
-	<meta
-		name="description"
-		content="Bitzquad creates next generation future-proof digital solutions for you."
-	/>
-	<meta
-		name="keywords"
-		content="bitzquad, bitsquad, software, solutions, website"
-	/>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="English" />
-	<meta name="author" content="Bitzquad" />
-
-	<!-- Open Graph / Facebook Meta   -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://bitzquad.com/" />
-	<meta property="og:title" content="Bitzquad | Software and IT Solutions" />
-	<meta
-		property="og:description"
-		content="Bitzquad creates next generation future-proof digital solutions for you."
-	/>
-	<meta
-		property="og:image"
-		content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
-	/>
-
-	<!-- Twitter Meta tags   -->
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content="https://bitzquad.com/" />
-	<meta
-		property="twitter:title"
-		content="Bitzquad | Software and IT Solutions"
-	/>
-	<meta
-		property="twitter:description"
-		content="Bitzquad creates next generation future-proof digital solutions for you."
-	/>
-	<meta
-		property="twitter:image"
-		content="https://cdn.discordapp.com/attachments/920056089670729728/920056217764761681/logo.png"
-	/>
-</svelte:head>
-
-<div>
-	<h1>{Txt.Msg[lang]}</h1>
-</div>
+<Navbar />
+<main>
+	<div
+		class="bz-container py-4 px-5 md:px-10 lg:px-20 2xl:py-20 bg-gray-100 text-gray-500"
+	>
+		<div
+			class="container max-w-7xl flex items-center mt-12 justify-center flex-col"
+		>
+			<h1 class="text-4xl lg:text-3xl 2xl:text-4xl  font-bold">
+				{statusCode === 404 ? Txt.Msg404[lang] : Txt.MsgOther[lang]}
+			</h1>
+			<p class="md:w-1/2 lg:text-sm text-center mt-5">
+				{statusCode === 404
+					? Txt.Description404[lang]
+					: Txt.DescriptionOther[lang]}
+			</p>
+			<div
+				class="md:relative w-full md:w-1/2 2xl:w-3/4 mt-10 mb-5 flex flex-col-reverse"
+			>
+				<a
+					href={Path("/")}
+					class="md:absolute my-10 md:mt-0 w-full md:w-auto md:top-0 md:left-1/2 flex justify-center  md:-translate-x-1/2 bg-yellow-400 text-yellow-700 p-3 px-4 rounded hover:text-gray-600 transition-all duration-300 shadow-lg hover:bg-yellow-300 hover:shadow-xl"
+					>Go To Home</a
+				>
+				<img
+					src={statusCode === 404 ? "/404-error.svg" : "/bug-fix.svg"}
+					alt="404-error"
+					class="w-full"
+				/>
+			</div>
+		</div>
+	</div>
+</main>
+<Footer />
